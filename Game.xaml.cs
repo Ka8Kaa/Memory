657558792dd51b8c14cf8f756efbd4bfd539ea42
Ga-delta <Ga-delta@SS-43-7>
2024-02-13 13:06:03 +0100

    Add project files.

---
+++ Game.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Memory
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Game : Page
+    {
+        public Game()
+        {
+            this.InitializeComponent();
+        }
+        private void MenuFlyoutItem_Click_5(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void Image_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_Tapped_1(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_Tapped_2(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_Tapped_3(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_Tapped_4(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_Tapped_5(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_Tapped_6(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_Tapped_7(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
